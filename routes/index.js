afad1605dbab6cfbb0a64e0ae941fd06b6d0ed75
mnpk <mwpark@castis.com>
2015-06-01 01:22:41 +0900

    about, api 페이지 추가

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,25 @@ var lorem = require('../lorem');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '한글 로렘 입숨' });
 });
 
 router.get('/about/', function(req, res) {
   res.render('about', { title: 'About' });
 });
 
+router.get('/api-guide/', function(req, res) {
+  res.render('api-guide', { title: 'API Guide' });
+});
+
 router.get('/api/lorem/', function(req, res, next) {
   var p = req.query.p;
   if (p === undefined) {
-    p = 3;
+    p = 2;
   }
   var l = req.query.l;
   if (l === undefined) {
-    l = 60;
+    l = 20;
   }
   res.json(lorem.lorem_json(p, l, req.query.c));
 });
@@ -26,11 +30,11 @@ router.get('/api/lorem/', function(req, res, next) {
 router.get('/api/lorem.txt/', function(req, res, next) {
   var p = req.query.p;
   if (p === undefined) {
-    p = 3;
+    p = 2;
   }
   var l = req.query.l;
   if (l === undefined) {
-    l = 60;
+    l = 20;
   }
   res.type('text/plain');
   res.send(lorem.lorem(p, l, req.query.c));
